e10406205074f6f702dcfd15c002663af70ff29e
Pass Automated Testing Suite <Pass-Automated-Testing-Suite@zx2c4.com>
2025-07-10 15:02:26 +0100

    Interpolate between chains, and lowercase all the data

--- src/markov.rs
+++ src/markov.rs
@@ -1,4 +1,7 @@
-use rand::{rng, seq::IndexedRandom};
+use rand::{
+    rng,
+    seq::{IndexedRandom, SliceRandom},
+};
 use std::{
     collections::{HashMap, VecDeque},
     fs::File,
@@ -7,27 +10,23 @@ use std::{
 
 pub struct Markov {
     n_gram: usize,
-    chain: HashMap<Vec<char>, Vec<char>>,
-    // chain: HashMap<Vec<String>, Vec<String>>,
+    chains: Vec<HashMap<Vec<char>, Vec<char>>>,
 }
 
 impl Markov {
     pub fn new(files: Vec<String>, n_gram: usize) -> Self {
         let mut markov = Self {
             n_gram,
-            chain: HashMap::new(),
+            chains: Vec::new(),
         };
 
         for file in files {
+            // Create a new chain for each file
+            let mut chain = HashMap::new();
+
             // Open a file to use as the data
             let f = File::open(file.as_str()).unwrap();
             let reader = BufReader::new(f);
-
-            // let words_iter = reader.lines().map_while(Result::ok).flat_map(|line| {
-            //     line.split_whitespace()
-            //         .map(String::from)
-            //         .collect::<Vec<_>>()
-            // });
 
             let char_iter = reader
                 .lines()
@@ -36,54 +35,77 @@ impl Markov {
 
             // Split the file contents into words
             let mut prev_n = VecDeque::new();
-            for current in char_iter {
-                // for current in words_iter {
+            for token in char_iter {
                 // Add to the chain or insert a new entry if the key has no associated chain yet
                 if !prev_n.is_empty() {
-                    markov
-                        .chain
+                    chain
                         .entry(prev_n.clone().into())
-                        .or_insert_with(|| vec![current.clone()])
-                        .push(current.clone());
+                        .or_insert_with(|| vec![token])
+                        .push(token);
                 }
 
                 // Rotate the previous entries so that there is only ever N_gram amount of entries in the previous N
                 if prev_n.len() < markov.n_gram {
-                    prev_n.push_back(current.clone());
+                    prev_n.push_back(token);
                 } else {
                     prev_n.pop_front();
-                    prev_n.push_back(current.clone());
+                    prev_n.push_back(token);
                 }
             }
+
+            // Add the chain to the chains Vec
+            markov.chains.push(chain);
         }
 
         markov
     }
 
     pub fn generate_text(&self, length: usize) -> String {
-        // Generate words from the chain
+        // Generate words from a random chain
         let mut rng = rng();
-        let keys = self.chain.keys().cloned().collect::<Vec<_>>();
+        let mut chain = self.chains.choose(&mut rng).unwrap();
+        let keys = chain.keys().cloned().collect::<Vec<_>>();
         let mut key: VecDeque<_> = keys.choose(&mut rng).unwrap().clone().into();
 
         let mut output: Vec<_> = key.clone().into();
 
+        // let mut chain = HashMap::new();
         for _ in 0..(length - self.n_gram) {
-            let Some(next_words) = self.chain.get(&Into::<Vec<_>>::into(key.clone())) else {
+            // Shuffle the indices of the chains, so that, when checking if there is a continuation, we don't check each chain more than once
+            let mut shuffled_indices = (0..self.chains.len()).collect::<Vec<_>>();
+            shuffled_indices.shuffle(&mut rng);
+
+            // Pick a random chain to continue the generation, choosing only chains which have a continuation
+            let mut next_tokens = None;
+            for chain_idx in shuffled_indices {
+                chain = &self.chains[chain_idx];
+
+                // Chain has a continuation
+                if let Some(next_tokens_in_chain) = chain.get(&Into::<Vec<_>>::into(key.clone())) {
+                    next_tokens = Some(next_tokens_in_chain);
+                    break;
+                }
+            }
+
+            // Exit the generation if no chains have a continuation
+            let Some(next_words) = next_tokens else {
                 break;
             };
 
+            // Randomly select a word to come next in the generation
             let Some(next) = next_words.choose(&mut rng) else {
                 break;
             };
 
-            output.push(next.clone());
+            // Add the next word to the output
+            output.push(*next);
 
+            // Rotate the previous N Vec
             key.pop_front();
-            key.push_back(next.clone());
+            key.push_back(*next);
         }
 
+        // Join the output into a String
         output.into_iter().collect()
-        // output.join(" ")
     }
 }
--- src/tts.rs
+++ src/tts.rs
@@ -5,13 +5,13 @@ pub fn text_to_speech(text: String) {
         .arg("--lang")
         .arg("en")
         .arg("--output")
-        .arg("assets/test.mp3")
+        .arg("assets/Text_To_Speech.mp3")
         .output()
         .unwrap();
 
     println!("About to speak");
 
-    let a = std::process::Command::new("mpv")
-        .arg("assets/test.mp3")
+    let _ = std::process::Command::new("mpv")
+        .arg("assets/Text_To_Speech.mp3")
         .spawn();
 }
